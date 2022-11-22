c51b4cc418c0d4a4043a3a5d193f9be449fd52f5
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2022-11-22 20:32:40 +0000

    Added code style enforcement

--- apis/brand-api/app/main.py
+++ apis/brand-api/app/main.py
@@ -5,10 +5,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
 
 @app.get("/")
 def read_root():
@@ -19,7 +21,7 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
-
