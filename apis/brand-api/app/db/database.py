565f1a1482cec131137e1637542a95c416728cb9
João Carlos Silva <jcvsilva@gmail.com>
2022-11-24 19:36:20 +0000

    Add install psycopg2-binary to Pipfile. Changed the postgres server n… (#2)
    Made pipenv install psycopg2-binary
    Changed the postgres server name to postgres in docker-compose

--- apis/brand-api/app/db/database.py
+++ apis/brand-api/app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = \
-    "postgresql://postgres:postgres@postgresserver/brand_db"
+    "postgresql://postgres:postgres@postgres/brand_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
