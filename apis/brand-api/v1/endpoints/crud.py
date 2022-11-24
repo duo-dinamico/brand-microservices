81bfc85efaa3d5c7797de81ab37cb10d01388b31
Joao Carlos Silva <jcvsilva@gmail.com>
2022-11-24 21:02:22 +0000

    Added the Brand model and much more.

--- apis/brand-api/v1/endpoints/crud.py
+++ apis/brand-api/v1/endpoints/crud.py
@@ -1,24 +1,25 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from core.models import main_models
+from core.schemas import schemas
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    return db.query(main_models.User).filter(main_models.User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+    return db.query(main_models.User).filter(main_models.User.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+    return db.query(main_models.User).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = \
-        models.User(email=user.email, hashed_password=fake_hashed_password)
+        main_models.User(email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -26,11 +27,11 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+    return db.query(main_models.Item).offset(skip).limit(limit).all()
 
 
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db_item = main_models.Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
