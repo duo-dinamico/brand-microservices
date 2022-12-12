5cb4e7bed555d5a3665f99c296f15f58b2179cc8
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2022-12-12 21:48:22 +0000

    Added poetry and changes to docker

--- apis/brand-api/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
-
-def test_error_root():
-    response = client.get("/")
-    assert response.status_code == 400
-    assert response.json() == {"msg": "Bad Request"}
