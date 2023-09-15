b1f801bb584d4be06677175e584fe9434bc24be6
Joao Jesus <67010899+joaojesus81@users.noreply.github.com>
2023-09-15 09:50:43 +0100

    Update main.py

--- apis/brand_api/main.py
+++ apis/brand_api/main.py
@@ -55,10 +55,14 @@ def validation_exception_handler(request, exc):
 
     return JSONResponse(response, status_code=422)
 
+origins = [
+    "http://127.0.0.1:5173/",
+    "http://localhost:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
